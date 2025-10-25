e45af0e2d97162cc93d3ed6741d95c73b26f33c5
Mesa <112003mesa@gmail.com>
2025-10-26 02:51:13 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// export default Vite config
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {port: 5173}
+  base: './',
+  build: {
+    outDir: 'dist',
+  },
 });
